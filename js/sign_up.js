d37c322e8d0967d70b6a15df9ff6c7502f76d88c
GDescroix <guydcx@gmail.com>
2021-09-08 15:56:53 +0200

    vérif ANSSI du mdp intégrée

--- js/sign_up.js
+++ js/sign_up.js
@@ -5,24 +5,41 @@ let mdp = document.querySelector(".mdp");
 
 btn.addEventListener("click", function () {
     if (u_name.value && user.value && mdp.value) {
-        fetch("http://musics.logikstik.odns.fr/api/users", {
-            method: "POST",
-            body: JSON.stringify({
-                name: u_name.value,
-                email: user.value,
-                password: mdp.value
-            }),
-            headers: {
-                'Content-type': 'application/json; charset=UTF-8',
-            }
-        })
-        .then(function (response) {
-            if (response.status != 401) {
-                u_name.style.border = "3px solid red";
-                user.style.border = "3px solid red";
-                mdp.style.border = "3px solid red";
-            }
-        })
+        var decimal = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,15}$/;
+
+        if (mdp.value.match(decimal)) {
+            let error_mdp = document.querySelector(".center");
+
+
+            u_name.style.border = "solid black 1.5px";
+            user.style.border = "solid black 1.5px";
+            mdp.style.border = "solid black 1.5px";
+            error_mdp.style.border = "none";
+            error_mdp.textContent = "";
+            fetch("http://musics.logikstik.odns.fr/api/users", {
+                    method: "POST",
+                    body: JSON.stringify({
+                        name: u_name.value,
+                        email: user.value,
+                        password: mdp.value
+                    }),
+                    headers: {
+                        'Content-type': 'application/json; charset=UTF-8',
+                    }
+                })
+                .then(function (response) {
+                    if (response.status != 201) {
+                        u_name.style.border = "3px solid red";
+                        user.style.border = "3px solid red";
+                        mdp.style.border = "3px solid red";
+                    }
+                })
+        } else {
+            let error_mdp = document.querySelector(".center");
+
+            error_mdp.style.border = "solid black 1.5px";
+            error_mdp.textContent = "Veuillez respecter le format de mot de passe de l'ANSSI (12 caractères avec une majuscule, une minuscule, un chiffre et un caractère spécial)";
+        }
     } else {
         u_name.style.border = "solid black 1.5px";
         user.style.border = "solid black 1.5px";
